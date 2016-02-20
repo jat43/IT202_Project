6d15eba07499d39396abde979cd17e34427f3863
Julio Torres <jat43@njit.edu>
2016-02-19 20:20:22 -0500

    got meh.php to create a table now i need it to work seamless from the html page i made >.<

--- meh.php
+++ meh.php
@@ -1,32 +1,52 @@
 #!/usr/bin/php
 <?php
 
-$db = new mysqli("localhost", "root", "kokopepe03", "IT202");
+$dbhost	= "localhost";
+$dbuser	= "root";
+$dbpass = "kokopepe03";
+$dbconn = mysql_connect( $dbhost, $dbuser, $dbpass);
 
-
-if ($db->connect_errno > 0)
+if (! $dbconn )
 {
-  echo __FILE__.__Line__." ERROR: ".$db->connect_error.PHP_EOL;
-  exit(-1);
+  die('Could not connect: ' . mysql_error());
 }
-
-
-
-$query = "select * from Student;";
-
-$results = $db->query($query);
-
-print_r($results);
-
-while ($row = $results->fetch_assoc())
-{
-  print_r($row);
-}
-
 
 echo "We are connected to the DATABASE".PHP_EOL;
 
-$db->close();
+$sql = "CREATE TABLE user_info(".
+    "idNum INT NOT NULL AUTO_INCREMENT, ".
+    "first_name VARCHAR(30), ".
+    "last_name VARCHAR(30), ".
+    "user_email VARCHAR(30), ".
+    "street_address VARCHAR(30), ".
+    "city_address VARCHAR(30), ".
+    "state_address VARCHAR(5), ".
+    "PRIMARY KEY ( idNum ));";
+    
+mysql_select_db( 'customerInfo' );
+
+$retval = mysql_query( $sql, $dbconn );
+if(! $retval)
+{
+  die('Could not create table: ' . mysql_error());
+}
+echo "You made the table bruh, congrats\n";
+
+//$query = "select * from Student;";
+
+//$results = $db->query($query);
+
+//print_r($results);
+
+//while ($row = $results->fetch_assoc())
+//{
+//  print_r($row);
+//}
+
+
+
+
+mysql_close($dbconn);
 
 /* populate a database in mysql from a php file 
 */
